f74b9c9332062fa349687b1c9609c1b79180d2cb
Clinton Enyinna <enyinnaclinton@gmail.com>
2022-02-15 02:04:19 -0600

    live search feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def search
+    if params[:search].present?
+      @articles = Article.pg_search(params[:search])
+    else
+      @articles = Article.all
+    end
+
+    render :search, layout: false
+  end
+
   def new
   end
 
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,6 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  connect() {
+  static targets = [ "formButton", "articles" ]
+
+  search() {
+    this.formButtonTarget.click()
+  }
+
+  updateArticles(e) {
+    const [data, status, xhr] = e.detail
+
+    this.articlesTarget.innerHTML = xhr.response
   }
 }
