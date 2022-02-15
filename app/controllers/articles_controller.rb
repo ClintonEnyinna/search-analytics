7848e26d119b752e3c69321ea4cb8f4cf426a160
Clinton Enyinna <enyinnaclinton@gmail.com>
2022-02-15 02:46:34 -0600

    create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,17 +14,22 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
-  def edit
-  end
+  private
 
-  def update
-  end
-
-  def destroy
+  def article_params
+    params.require(:article).permit(:title, :author, :body)
   end
 end
