b040df2ec751e148dafefb3b0c036346191629e7
Clinton Enyinna <enyinnaclinton@gmail.com>
2022-02-17 19:48:38 -0600

    remove after_create callback in user model, downcase before save in search_analytic model

--- app/models/search_analytic.rb
+++ app/models/search_analytic.rb
@@ -1,3 +1,5 @@
 class SearchAnalytic < ApplicationRecord
   belongs_to :user
+
+  before_save { query.downcase! }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,4 @@ class User < ApplicationRecord
   has_many :search_analytics
 
   validates :name, :email, presence: true
-
-  after_create :default_search_analytic
-
-  private
-
-  def default_search_analytic
-    SearchAnalytic.create!(query: "", user: self)
-  end
 end
